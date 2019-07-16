8ca3ef2c69f03d860bbb5a08e0c4f2cdd4b954ed
moosekiss <gusdn6526@gmail.com>
2019-07-17 00:14:13 +0900

    Changing Color Function

--- app.js
+++ app.js
@@ -1,24 +1,56 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+ 
 let painting = false;
 
 function stopPainting() {
     painting = false;
 }
 
-function onMouseMove(event) {
-    const x = event.offsetX;
-    const y = event.offsetY;
-}
-
-function onMouseDown(event) {
+function startPainting() {
     painting = true;
 }
 
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+
+    if(!painting) {
+        //console.log("creating path in, " +  x, y);
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        //console.log("creating line in, " +  x, y);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+}
 
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+// change colors
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
+
+/*
+canvas API에 대한 이해가 필요함
+CanvasRenderingContext2D
+
+*/ 
