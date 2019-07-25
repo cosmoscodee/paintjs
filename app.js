163f4492e620d7ba28879c2b1c6e815bddd3d98b
moosekiss <gusdn6526@gmail.com>
2019-07-25 23:22:11 +0900

    End of PaintJS

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const canvas = document.getElementById("jsCanvas");
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");    // CanvasRenderingContext2D API
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = "#2c2c2c";
-ctx.lineWidth = 2.5;
- 
 let painting = false;
+let filling = false;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
+ctx.lineWidth = 2.5;
+
+
 
 function stopPainting() {
     painting = false;
@@ -33,10 +45,45 @@ function onMouseMove(event) {
     }
 }
 
-
 function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+
+function handleRangeChange(event) {
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+    if(filling === true) {
+        filling = false;
+        mode.innerText = "Fill";
+    } else {
+        filling = true;
+        mode.innerText = "Paint";       
+    }
+}
+
+function handleCanvasClick() {
+    if(filling) {
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+}
+
+// 우클 방지
+function handleCM(event) {
+    event.preventDefault();
+}
+
+function handleSaveClick() {
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS[EXPORT]";
+    link.click();
 }
 
 if(canvas) {
@@ -44,13 +91,26 @@ if(canvas) {
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
 }
 
 // change colors
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
 
+if(range) {
+    range.addEventListener("input", handleRangeChange);
+}
+
+if(mode) {
+    mode.addEventListener("click", handleModeClick);
+}
+
+if(saveBtn) {
+    saveBtn.addEventListener("click", handleSaveClick);
+}
+
 /*
 canvas API에 대한 이해가 필요함
 CanvasRenderingContext2D
-
 */ 
